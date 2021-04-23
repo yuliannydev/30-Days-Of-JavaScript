aa2e73d06c86594efe098e2ff7f82791b6fa8bbd
Yulianny Betancourt <flipsdulce@gmail.com>
2021-04-23 02:23:26 -0400

    Termine el dia 5

---
+++ Day-5/examples.js
@@ -0,0 +1,102 @@
+//If we try modify the string stored in variable word, JavaScript should raise an error.
+
+/* let word = 'JavaScript';
+
+word[0] = 'Y';
+
+let word = 'Other';
+
+console.log(word); */
+
+
+/* let word = "JavaScript";
+
+//word[0] = "Y";
+
+let word = "Other";
+
+console.log(word)
+ */
+/* function ShowWord(){
+    var word = "Make";
+    console.log(word);
+}
+
+ShowWord(); */
+
+
+//Non-primitive data types in JavaScript includes:
+
+/**
+ * 1. Objects
+ * 2. Functions
+ * 3. Arrays
+ */
+
+//Examples with array
+
+/* array = [1, 2, 3];
+
+array[0] = 2;
+
+console.log(array); */
+
+/* let numOne = 3;
+let numTwo = 3;
+
+console.log(numOne == numTwo); */
+
+/* let js = "JavaScript";
+let py = "Javascript";
+
+console.log(js == py); */
+
+/* let user = {
+    name: 'Yulianny',
+    lastName: 'Betancourt',
+    age: 31
+}
+
+let admin = {
+    name: 'Yulianny',
+    lastName: 'Betancourt',
+    age: 31
+}
+
+let admin = user;
+
+console.log(user == admin); */
+
+/* const PI = Math.PI;
+
+let value = PI * 2;
+console.log(value); */
+
+//array = [2, 0, 22];
+
+/* console.log(Math.min(2, 0, 22)); */
+
+/* let randomNum = Math.random();
+let numBtn = randomNum * 11;
+
+console.log(numBtn); */
+
+/* let quote = "The saying, See is nnn";
+
+console.log(quote); */
+
+/* let nombre = 'Yulianny';
+let lastName = 'Betancourt';
+let age = 31;
+let space = ' ';
+
+let fullName = nombre + space + lastName + space + age;
+
+console.log(fullName); */
+
+/* let sentence = 'Esto es un ejemplo de \
+una oracion larga \
+con long literal string \
+y backslash';
+
+console.log(sentence); */
